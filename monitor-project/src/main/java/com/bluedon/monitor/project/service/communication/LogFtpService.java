424f9021cc95679d443be6850e7eff3893f488c6
fengzhihui <495300897@qq.com>
2017-11-21 17:44:06 +0800

    修改

--- monitor-project/src/main/java/com/bluedon/monitor/project/service/communication/LogFtpService.java
+++ monitor-project/src/main/java/com/bluedon/monitor/project/service/communication/LogFtpService.java
@@ -1,11 +1,12 @@
 package com.bluedon.monitor.project.service.communication;
 
 import java.util.List;
+import java.util.Map;
 
 import com.bluedon.monitor.project.entity.transferTable.TransferTable;
 
 public interface LogFtpService {
 	String getLatestFtpDatetime();
 	List<TransferTable> getTableList();
-	
+	List<Map<String, String>> getLogFtpPageList(String ftpIp,String fileName,String startT1,String startT2);
 }
--- monitor-project/src/main/java/com/bluedon/monitor/project/service/communication/impl/LogFtpServiceImpl.java
+++ monitor-project/src/main/java/com/bluedon/monitor/project/service/communication/impl/LogFtpServiceImpl.java
@@ -32,5 +32,22 @@ public class LogFtpServiceImpl implements LogFtpService {
 	public List<TransferTable> getTableList() {
 		return hibernateDao.queryForList("SELECT t FROM TransferTable t WHERE t.stat = 0", new Object[]{}, null);
 	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Map<String, String>> getLogFtpPageList(String ftpIp,String fileName,String startT1,String startT2) {
+		String sql = "SELECT a.FTP_IP FTP_IP,a.FILENAME,"
+						+ "MAX(CASE a.OPER_CODE WHEN 0 THEN a.num ELSE 0 END) SUCCESS,"
+						+ "MAX(CASE a.OPER_CODE WHEN 1 THEN a.num ELSE 0 END) FAILURE "
+					+ "FROM "
+					+ "("
+						+ "SELECT w.FTP_IP,w.FILENAME,w.OPER_CODE,w.START_DATETIME,COUNT(0)num "
+						+ "FROM cm_log_ftp_dt w WHERE w.OPER_CODE=0 OR w.OPER_CODE=1 GROUP BY w.FTP_IP,w.OPER_CODE "
+					+ ")a "
+					+ "WHERE a.FTP_IP LIKE '%ftpIp%' AND a.FILENAME LIKE '%fileName%' AND (a.START_DATETIME BETWEEN 'T1' AND 'T2') "
+					+ "GROUP BY a.FTP_IP;";
+		sql = sql.replace("ftpIp", ftpIp).replace("fileName", fileName).replace("T1", startT1).replace("T2", startT2);
+		return hibernateDao.selectBySql(sql);
+	}
 	
 }
