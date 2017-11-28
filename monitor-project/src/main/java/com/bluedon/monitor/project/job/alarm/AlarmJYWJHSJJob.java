e892b00cfe9e1c82d8186b401c96bd2cc054270d
ming <664219802@qq.com>
2017-11-28 11:34:33 +0800

    zhouming

--- monitor-project/src/main/java/com/bluedon/monitor/project/job/alarm/AlarmJYWJHSJJob.java
+++ monitor-project/src/main/java/com/bluedon/monitor/project/job/alarm/AlarmJYWJHSJJob.java
@@ -5,9 +5,13 @@ package com.bluedon.monitor.project.job.alarm;
  */
 
 import com.bluedon.monitor.common.util.SendMailUtil;
+import com.bluedon.monitor.project.entity.alarm.AlarmNotice;
+import com.bluedon.monitor.project.service.alarm.IAlarmNoticeManagerService;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 import java.text.SimpleDateFormat;
@@ -21,8 +25,15 @@ public class AlarmJYWJHSJJob implements Job {
 
     public static String JOB_NAME = "alarm_jywjhsj_job";
 
+    @Autowired
+    @Qualifier("alarmNoticeServiceImpl")
+    private IAlarmNoticeManagerService iAlarmNoticeManagerService;
+
     @Override
     public void execute(JobExecutionContext arg0) throws JobExecutionException {
-        SendMailUtil.getInstance().doSendHtmlEmail("交易文件和数据", "test test test <a href='www.baidu.com'>百度</a>", "664219802@qq.com");
+        //AlarmNotice alarmNotice = new AlarmNotice();
+        //alarmNotice.setNoticeName("交易文件和数据");
+        //iAlarmNoticeManagerService.add(alarmNotice);
+        //SendMailUtil.getInstance().doSendHtmlEmail("交易文件和数据", "test test test <a href='www.baidu.com'>百度</a>", "664219802@qq.com");
     }
 }
--- monitor-project/src/main/java/com/bluedon/monitor/project/job/alarm/AlarmJYZXTJob.java
+++ monitor-project/src/main/java/com/bluedon/monitor/project/job/alarm/AlarmJYZXTJob.java
@@ -5,9 +5,13 @@ package com.bluedon.monitor.project.job.alarm;
  */
 
 import com.bluedon.monitor.common.util.SendMailUtil;
+import com.bluedon.monitor.project.entity.alarm.AlarmNotice;
+import com.bluedon.monitor.project.service.alarm.IAlarmNoticeManagerService;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -20,9 +24,16 @@ public class AlarmJYZXTJob implements Job {
 
     public static String JOB_NAME = "alarm_jyzxt_job";
 
+    @Autowired
+    @Qualifier("alarmNoticeServiceImpl")
+    private IAlarmNoticeManagerService iAlarmNoticeManagerService;
+
 
     @Override
     public void execute(JobExecutionContext arg0) throws JobExecutionException {
-        SendMailUtil.getInstance().doSendHtmlEmail("交易子系统", "test test test <a href='www.baidu.com'>百度</a>", "664219802@qq.com");
+        //AlarmNotice alarmNotice = new AlarmNotice();
+        //alarmNotice.setNoticeName("交易子系统");
+        //iAlarmNoticeManagerService.add(alarmNotice);
+        //SendMailUtil.getInstance().doSendHtmlEmail("交易子系统", "test test test <a href='www.baidu.com'>百度</a>", "664219802@qq.com");
     }
 }
